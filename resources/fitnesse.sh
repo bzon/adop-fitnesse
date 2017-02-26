9b5196fb0421d8ac365f92384a4fccec1896dabc
John Bryan Sazon <john.bryan.j.sazon@accenture.com>
2017-02-27 07:45:24 +0800

    Added a function to add Fitnesse runtime opts

--- resources/fitnesse.sh
+++ resources/fitnesse.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
 if [ ! -d $FITNESSE_HOME/FitNesseRoot ]; then
-    cp -rp /usr/share/fitnesse/ref/** $FITNESSE_HOME
+  cp -rp /usr/share/fitnesse/ref/** $FITNESSE_HOME
 fi
 
-cd $FITNESSE_HOME
+# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+if [[ ${#@} -lt 1 ]]; then
+  exec java -jar fitnesse-standalone.jar -p 8080 $FITNESSE_OPTS
+fi
 
-java -jar fitnesse-standalone.jar -p 8080 -v -o
+# As argument is not fitnesse, assume user want to run his own process, for example a `bash` shell to explore this image
+exec "$@"
+
