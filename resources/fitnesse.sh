1ece33404c235f1417834afea65a7509a3fbd3ec
John Bryan Sazon <john.bryan.j.sazon@accenture.com>
2017-02-27 18:58:41 +0800

    corrected the comments

--- resources/fitnesse.sh
+++ resources/fitnesse.sh
@@ -4,7 +4,6 @@ if [ ! -d $FITNESSE_HOME/FitNesseRoot ]; then
   cp -rp /usr/share/fitnesse/ref/** $FITNESSE_HOME
 fi
 
-# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ ${#@} -lt 1 ]]; then
   exec java -jar fitnesse-standalone.jar -p 8080 $FITNESSE_OPTS
 fi
